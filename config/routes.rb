9f0074cfaf988c1a4d2e7c830b0fdd08586269b5
Liew Yih Jiun <liewyj619@gmail.com>
2016-07-19 14:37:42 +0800

    fb login

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
       only: [:create, :edit, :update]
   end
 
+  resources :users, only: [:show, :edit, :update, :destroy] 
+
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
